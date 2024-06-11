b6a047c0c73ad170110291340e995cb1a57c57d8
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-11 10:55:25 +0900

    객체 동적 할당

--- app.js
+++ app.js
@@ -11,12 +11,29 @@ const formComponent = (action, method) => {
   return `
   <form action="${action} method=${method}">
     <label for="name">이름:</label>
-    <input type="text" id="name" name="name" value="" required>
+    <input type="text" id="name" name="name" value="${userInfo.name}" required>
     <label for="age">나이:</label>
-    <input type="number" id="age" name="age" value="" required>
+    <input type="number" id="age" name="age" value="${userInfo.age}" required>
     <label for="job">직업:</label>
-    <input type="text" id="job" name="job" value="" required>
+    <input type="text" id="job" name="job" value="${userInfo.job}" required>
     <input type="submit" value="제출">
   </form>
+  `;
+}
+
+const App = (formAction, formMethod) => {
+  `
+  <h1>함수의 리턴은</h1>
+  <h2>사람의 머리를 맑게 해준다.</h2>
+  ${formComponent(formAction, formMethod)}
   `
 }
+
+const renderApp = (formAction, formMethod) => {
+  const appDiv = document.getElementById('root');
+  appDiv.innerHTML = App(formAction, formMethod);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  renderApp('/submit', 'post');
+})
--- template.js
+++ template.js
@@ -1,11 +1,13 @@
-const a = () => {
+const a = (name, age, job) => {
   return {
-    name : '',
-    age : 0,
-    job : ''
+    name : name,
+    age : age,
+    job : job
   }
 }
 
 // console.log(a());
-const b = a();
-console.log(b);
+const b = a('유호영', 28, '학생');
+console.log('첫 번째 b:', b);
+b.address = '대전';
+console.log('두 번째 b:', b);
