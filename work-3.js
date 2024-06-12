7f308ea1b250001182a84f15b3a1bdd045801f83
Hoyeong Yoo <crystal23733@gmail.com>
2024-06-12 11:43:03 +0900

    생성자 함수 연습중

--- work-3.js
+++ work-3.js
@@ -1,24 +1,68 @@
-const basicData = () => {
-  return {
-    name : "유호영",
-    age: 28,
-    job : "학생"
+class BasicData {
+  constructor(name, age, job) {
+    this.name = name;
+    this.age = age;
+    this.job = job;
+  }
+  set name(value){
+    if(typeof value === 'string'){
+      this._name = value;
+    }
+  }
+  get name(){
+    return this._name;
+  }
+  set age(value){
+    if(typeof value === 'number'){
+      this._age = value;
+    }
+  }
+  get age(){
+    return this._age;
+  }
+  set job(value){
+    if(typeof value === 'string'){
+      this._job = value;
+    }
+  }
+  get job(){
+    return this._job;
   }
 }
+
+const objContent = (tagName, text) => {
+  for(const value in text){
+    return `<${tagName}>${text[value]}</${tagName}>`;
+  }
+}
+
+
 
 const component = () => {
-  let obj = basicData;
-
-  const decision = () => {
-    
+  let obj = new BasicData('유호영', 28, '학생');
+  
+  const decision = (obj) => {
+    if(obj instanceof BasicData === true){
+      return true;
+    } else {
+      return false;
+    }
   }
+  
+  console.log(objContent('h1', obj));
+  if(decision(obj) === 'true'){
+    console.log('성공')
+    // return `
+    //   <html>
+    //     <head>
+    //       <meta charset='utf-8'>
+    //     </head>
+    //     <body>
 
-  return `
-  <html>
-    <head>
-    </head>
-    <body>
-    </body>
-  </html>
-  `;
+    //     </body>
+    //   </html>
+    //   `;
+  } 
 }
+
+component();
